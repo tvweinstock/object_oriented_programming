b31a0384e107212d1ddc20073624ac9b4f6a98a9
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-14 15:37:09 -0400

    Pushing solution for people.rb

--- people.rb
+++ people.rb
@@ -4,24 +4,30 @@ class Person
 		@name = name
 	end
 
+	def greeting
+		puts "Hi, my name is #{@name}."
+	end
+end
 
 class Student
+
 	def learn
 		puts "I get it!"
 	end
 end
 
 class Instructor
+	def initialize
+	end
+
 	def teach
 		puts "Everything in Ruby is an Object."		
 	end
 end
 
-	def greeting
-		puts "Hi, my name is #{@name}."
-	end
 
-end #end of class Person
+
+# end #end of class Person
 
 instructor1 = Person.new("Chris")
 instructor1.name = "Chris"
@@ -32,6 +38,12 @@ student1 = Person.new("Cristina")
 student1.name = "Cristina"
 student1.greeting
 
+
 Instructor.new.teach
 Student.new.learn
+
+#calling the teach method on Student does not work because it in not defined within the Student Class.
+Student.new.teach
+
+
 
