ebd86ef23fb0363f7327fb348e8eb906624691c8
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-14 15:23:30 -0400

    First draft of excercise one in OOP intro

---
+++ people.rb
@@ -0,0 +1,37 @@
+class Person
+	attr_accessor :name
+	def initialize(name)
+		@name = name
+	end
+
+
+class Student
+	def learn
+		puts "I get it!"
+	end
+end
+
+class Instructor
+	def teach
+		puts "Everything in Ruby is an Object."		
+	end
+end
+
+	def greeting
+		puts "Hi, my name is #{@name}."
+	end
+
+end #end of class Person
+
+instructor1 = Person.new("Chris")
+instructor1.name = "Chris"
+instructor1.greeting
+
+
+student1 = Person.new("Cristina")
+student1.name = "Cristina"
+student1.greeting
+
+Instructor.new.teach
+Student.new.learn
+
