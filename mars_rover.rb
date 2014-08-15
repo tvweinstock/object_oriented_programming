d2de35a721c85bdd20d366617a80be82174c2ff3
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-15 15:28:02 -0400

    left turns fixed

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,5 +1,6 @@
 class Rover
 	attr_accessor :x, :y, :direction
+
 	def initialize (x, y, direction)
 		@x = x
 		@y = y
@@ -11,60 +12,65 @@ class Rover
 	end
 
 	def move(moves)
-		moves.each do|move|
+
+		move_array = moves.split(",")
+		move_array.each do |move|
+
 			case move
 			when 'R'
-				@direction = 'R'
+				turn_right
 			when 'L'
-				@direction = 'L'
+				turn_left
 			when 'M'
 				move_position
 			end
 		end
+	end	
 
-		def move_position
-			case@direction
-			when 'W'
-				@x -= 1
-			when 'S'
-				@x -= 1
-			when 'N'
-				@x += 1
-			when 'E'
-				@x += 1
-			end
-		end
-
-		def turn_right
-			case @direction
-			when 'N'
-				then @direction = 'E'
-			when 'E'
-				then @direction = 'S'
-			when 'S'
-				then @direction = 'W'
-			when 'W'
-				then @direction = 'N'
-			end
-		end
-
-		def turn_left
-			case @direction
-			when 'N'
-				then @direction = 'W'
-			when 'W'
-				then @direction = 'S'
-			when 'S'
-				then @direction = 'E'
-			when 'E'
-				then @direction = 'N'
-			end
+	def move_position
+		puts "hey there"
+		case @direction
+		when 'W'
+			@x -= 1
+		when 'S'
+			@y -= 1
+		when 'N'
+			@y += 1
+		when 'E'
+			@x += 1
 		end
 	end
+
+	def turn_right
+		case @direction
+		when 'N'
+			then @direction = 'E'
+		when 'E'
+			then @direction = 'S'
+		when 'S'
+			then @direction = 'W'
+		when 'W'
+			then @direction = 'N'
+		end
+	end
+
+	def turn_left
+		case @direction
+		when 'N'
+			then @direction = 'W'
+		when 'W'
+			then @direction = 'S'
+		when 'S'
+			then @direction = 'E'
+		when 'E'
+			then @direction = 'N'
+		end
+	end
+	
 end
 
 rover1 = Rover.new(1,2,'N')
-rover1.move("LMLMLMLMLMLM")
+rover1.move("R")
 puts rover1.current_position
 
 
