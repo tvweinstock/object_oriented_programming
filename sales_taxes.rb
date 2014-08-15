110a0933591ac89cbbceca5b15532ca244992847
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-15 09:49:31 -0400

    sales_tax calculator.rb created. Rover and Calculator still a work in progress

---
+++ sales_taxes.rb
@@ -0,0 +1,20 @@
+class SalesTax
+	attr_accessor :item :cost
+	def initialize (item, cost)
+		@item = item
+		@cost = cost
+	end
+
+	def basic_tax
+		
+	end
+
+	def import_duty
+		
+	end
+
+
+
+
+end
+
