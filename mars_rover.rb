8f83578170683a6727b9d0a54f2f4ef586ce27a7
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-14 23:38:47 -0400

    getting closer to moving the rover

--- mars_rover.rb
+++ mars_rover.rb
@@ -4,26 +4,48 @@ class Rover
 		@x = x
 		@y = y
 		@direction = direction
-
-		def current_position
-			"#{x}, #{y}, #{direction}"
-		end
-			
-		def turn #to change direction depending use if statement
-			if direction = north
-		end
-		
-		def move #to moved position + and -
-		end
-
-		def controller #to take in coordinates from NASA 		
-
-		end
-		
 	end
 
+	def current_position
+		"#{x}, #{y}, #{direction}"
+	end
+
+	def move(moves)
+		moves.each do|move|
+			case move
+			when 'R'
+				@direction = 'R'
+			when 'L'
+				@direction = 'L'
+			when 'M'
+				move_forward
+			end
+		end
+
+			def move_forward
+				case@direction
+				when 'W'
+					@x -= 1
+				end
+			end
+
+				def turn_right
+					@direction = case @direction
+					when 'N'
+						'E'
+					when 'E'
+						'S'
+					when 'S'
+						'W'
+					when 'W'
+						'N'
+					end
+				end
+	end
 end
 
 rover1 = Rover.new(1,2,'N')
+rover1.move(1,4,'N')
+
 
 
