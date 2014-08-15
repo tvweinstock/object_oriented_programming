58eeb5d99fed6b476f9182c1fbe5474fb3edd30e
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-15 17:40:00 -0400

    added calculations for sales tax

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,20 +1,45 @@
-class SalesTax
-	attr_accessor :item :cost
-	def initialize (item, cost)
-		@item = item
-		@cost = cost
+class Product 
+	attr_accessor :quantity, :item, :price
+	def initialize (quantity, item, price)
+		@quantity = quantity
+		@item     = item
+		@price    = price
 	end
 
-	def basic_tax
+	def print_list(list)
+		list.each do |item|
+			puts "#{@quantity}, #{@item}, #{@price}"
+		end
+	end
+
+	puts "Enter item name"
+	new_item = gets.chomp
+	if new_item = "book" || "food" || "medical products"
+		puts 'No tax applied to this item'
 		
 	end
-
-	def import_duty
-		
-	end
-
-
-
 
 end
+
+class ProductBasicTax < Product
+	def basic_tax(tax)
+		tax = @price * 0.1
+
+	end
+
+end
+
+class ProductImportDuty < Product
+	def import_duty(duty)
+		duty = @price * 0.05
+
+	end
+end
+
+
+
+class Receipt
+end
+
+book = Product.new(1, 'book', 12.49)
 
